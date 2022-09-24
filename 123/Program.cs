10b02e9dde9193e261dd04dcb80ea8f7bf5dc71f
emirm35 <emirmrt35@gmail.com>
2022-09-24 14:12:52 +0300

    Proje dosyası ekle.

---
+++ 123/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123
+{
+
+     
+
+
+    internal class Program
+    {
+         
+      
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("mustang","Red",1969,20000);
+            Car BMW = new Car("m760li", "black", 2023, 1600000);
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
