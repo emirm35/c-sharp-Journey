07c92f6d9b53c1588adb007a1dfa4010b28d0fb8
emirm35 <emirmrt35@gmail.com>
2022-09-25 09:29:08 +0300

    Multiple interfaces

--- 123/Animal.cs
+++ 123/Animal.cs
@@ -6,19 +6,34 @@ using System.Threading.Tasks;
 
 namespace _123
 {
-    interface IAnimal
-    {
-        void animalSound();
-    }
-    class Pig : IAnimal
-    {
-        public void animalSound()
-        {
-            // The body of animalSound() is provided here
-            Console.WriteLine("The Pig sayS:wee wee");
-        }
+    //interface IFirstInterface
+    //{
+    //    void myMethod(); // interface method
+    //}
 
-    }
+    //interface ISecondInterface
+    //{
+    //    void myOtherMethod(); // interface method
+    //}
+
+
+    //class DemoClass : IFirstInterface, ISecondInterface
+    //{
+    //    public void myMethod()
+    //    {
+    //        Console.WriteLine("Some text...");
+    //    }
+
+
+    //    public void myOthermethod()
+    //    {
+    //        Console.WriteLine("Some other text...");
+    //    }
+
+    //}
+
+
+
 
 
 
--- 123/Program.cs
+++ 123/Program.cs
@@ -7,30 +7,52 @@ using System.Threading.Tasks;
 namespace _123
 {
 
-     
+
+
+    interface IFirstInterface
+    {
+        void myMethod(); // interface method
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod(); // interface method
+    }
+
+    // Implement multiple interfaces
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Some text..");
+        }
+        public void myOtherMethod()
+        {
+            Console.WriteLine("Some other text...");
+        }
+    }
+
+
+
+
+
+
+
 
 
     internal class Program
-    {
-         
-      
+    { 
+
 
         static void Main(string[] args)
         {
            
 
-            Pig myPig = new Pig();
 
-            myPig.animalSound();
+            DemoClass myObj = new DemoClass();
 
-
-
-
-
-
-
-
-
+            myObj.myMethod();
+            myObj.myOtherMethod();
 
 
 
